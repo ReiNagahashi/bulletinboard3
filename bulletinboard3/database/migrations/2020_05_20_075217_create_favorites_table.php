148631a6e33aec3ae5f2bb8e61387f6ca783350c
Rei Nagahashi <nagahashi.bhsoft@gmail.com>
2020-05-27 17:06:20 +0700

    commit fot adding limitation

--- bulletinboard3/database/migrations/2020_05_20_075217_create_favorites_table.php
+++ bulletinboard3/database/migrations/2020_05_20_075217_create_favorites_table.php
@@ -15,9 +15,20 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('post_id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+
+            $table->foreign('post_id')
+                  ->references('id')
+                  ->on('posts')
+                  ->onDelete('cascade');
+                 
         });
     }
 
